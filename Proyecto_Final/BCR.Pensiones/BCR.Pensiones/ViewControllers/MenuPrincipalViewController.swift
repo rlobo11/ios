83959242eb94d65d58f7a63649e567e326250f67
BCR <bcr@estacion-9.ucenfotec.local>
2018-05-19 13:28:47 -0600

    commit 190518

---
+++ Proyecto_Final/BCR.Pensiones/BCR.Pensiones/ViewControllers/MenuPrincipalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BCR.Pensiones
+//
+//  Created by BCR on 16/5/18.
+//  Copyright © 2018 laurs. All rights reserved.
+//
+
+import UIKit
+
+class MenuPrincipalViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ Proyecto_Final/BCR.Pensiones/BCR.Pensiones/ViewControllers/PoderDatosPersonalesViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  BCR.Pensiones
+//
+//  Created by BCR on 16/5/18.
+//  Copyright © 2018 laurs. All rights reserved.
+//
+
+import UIKit
+
+class PoderDatosPersonalesViewController: UIViewController {
+    
+    //textbox
+    @IBOutlet weak var txtIdentificacion: UITextField!
+    @IBOutlet weak var txtNombre: UITextField!
+    @IBOutlet weak var txtPrimerApellido: UITextField!
+    @IBOutlet weak var txtSegundoApellido: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
